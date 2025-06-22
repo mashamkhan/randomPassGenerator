fc041b6836d4531895190a62861301e1f5e252fe
mashamkhan <masham7776@gmail.com>
2025-06-22 17:39:00 +0500

    first commit

---
+++ file.js
@@ -0,0 +1,15 @@
+function generatePassword(length) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+
+  return password;
+}
+
+// Example usage:
+const password = generatePassword(12); // change the number to get different lengths
+console.log("🔐 Your Random Password is:", password);
